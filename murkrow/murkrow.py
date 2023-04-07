385d7a04a7480fbf5e5d80a90865912d6b6d6a12
Kyle Kelley <rgbkrk@gmail.com>
2023-04-06 17:29:41 -0700

    include our basic murkrow helper

--- murkrow/murkrow.py
+++ murkrow/murkrow.py
@@ -1,1 +1,37 @@
 """Main module."""
+
+import openai
+
+from .messaging import deltas, ai, human
+from .display import Markdown
+
+
+class Murkrow:
+    def __init__(self, *initial_context, model="gpt-3.5-turbo"):
+        if initial_context is None:
+            initial_context = []
+
+        self.messages = []
+        self.messages.extend(initial_context)
+        self.model = model
+
+    def chat(self, *messages):
+        # Messages are either a dict respecting the {role, content} format or a str that we convert to a human message
+        for message in messages:
+            if isinstance(message, str):
+                self.messages.append(human(message))
+            else:
+                self.messages.extend(messages)
+
+        mark = Markdown()
+        mark.display()
+
+        resp = openai.ChatCompletion.create(
+            model=self.model,
+            messages=self.messages,
+            stream=True,
+        )
+
+        mark.extend(deltas(resp))
+
+        self.messages.append(ai(mark.message))
