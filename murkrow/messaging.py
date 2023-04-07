c1ff094601ed644498f4786794b064543bf0e490
Kyle Kelley <rgbkrk@gmail.com>
2023-04-06 17:19:10 -0700

    include our basic chatterbox

--- murkrow/messaging.py
+++ murkrow/messaging.py
@@ -1,4 +1,35 @@
-# OpenAI role helpers
+import openai
+from typing import Any, Dict, Iterator, List, TypedDict
+
+
+Delta = TypedDict(
+    "Delta",
+    {
+        "content": str,
+    },
+)
+
+
+StreamChoice = TypedDict(
+    "StreamChoice",
+    {
+        "delta": Delta,
+    },
+)
+
+StreamCompletion = TypedDict(
+    "StreamCompletion",
+    {
+        "choices": List[StreamChoice],
+    },
+)
+
+
+def deltas(completion: Iterator[StreamCompletion]) -> Iterator[str]:
+    for chunk in completion:
+        delta = chunk["choices"][0]["delta"]
+        if "content" in delta:
+            yield delta["content"]
 
 
 def assistant(message):
@@ -33,3 +64,29 @@ def human(message):
 
 def ai(message):
     return assistant(message)
+
+
+class Murkrow:
+    def __init__(self, *initial_context):
+        if initial_context is None:
+            initial_context = []
+
+        self.messages = []
+        self.messages.extend(initial_context)
+        self.model = "gpt-3.5-turbo"
+
+    def chat(self, *messages):
+        self.messages.extend(messages)
+
+        mark = Markdown()
+        mark.display()
+
+        resp = openai.ChatCompletion.create(
+            model=self.model,
+            messages=self.messages,
+            stream=True,
+        )
+
+        mark.extend(deltas(resp))
+
+        self.messages.append(ai(mark.message))
