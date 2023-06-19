fe2f1ce31275648a9fc1a37a042d2d993e8cbc35
Kyle Kelley <rgbkrk@gmail.com>
2023-06-19 11:02:52 -0700

    Bump version: 0.11.3 → 0.11.4

--- murkrow/__init__.py
+++ murkrow/__init__.py
@@ -15,7 +15,7 @@ I am the king of the skies, the lord of the avian realm. Squawk!
 
 __author__ = """Kyle Kelley"""
 __email__ = 'rgbkrk@gmail.com'
-__version__ = '0.11.3'
+__version__ = '0.11.4'
 
 
 # Export Markdown from display
