e67ed5c049e220419f5e5691b4ed33eb2bd833e4
Kyle Kelley <rgbkrk@gmail.com>
2023-06-21 14:40:52 -0700

    create a built in IPython run cell function

---
+++ murkrow/builtins.py
@@ -0,0 +1,23 @@
+"""Builtins for Murkrow."""
+from IPython.core.getipython import get_ipython
+
+
+def run_cell(code: str):
+    """Run Python code in the IPython kernel.
+
+    Sometimes the GPT Models hallucinate a function called `python`. This is a
+    rudimentary implementation of that function relying on IPython.
+
+    This could be further expanded to match what is in dangermode for an IPythonic combination
+    of stdout, stderr, exceptions, and rich output.
+    """
+    ip = get_ipython()
+    if ip is None:
+        raise Exception("Could not get IPython instance")
+
+    try:
+        output = ip.run_cell(code, silent=True)
+        return output.result
+    except Exception as e:
+        # We want the exception to be returned to the LLM
+        return e
