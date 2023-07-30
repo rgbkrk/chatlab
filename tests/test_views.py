e3453e6b73aee93cf7207efe30c3bc48b11c47e6
Kyle Kelley <rgbkrk@gmail.com>
2023-07-29 18:48:56 -0700

    test views in a basic way

---
+++ tests/test_views.py
@@ -0,0 +1,41 @@
+# flake8: noqa
+import pytest
+
+from chatlab.views.assistant import AssistantMessageView
+from chatlab.views.markdown import Markdown
+
+
+def test_assistant_message_view_creation():
+    amv = AssistantMessageView()
+    assert isinstance(amv, AssistantMessageView)
+
+
+def test_markdown_creation():
+    md = Markdown()
+    assert isinstance(md, Markdown)
+
+
+def test_assistant_message_view_methods():
+    amv = AssistantMessageView()
+    try:
+        buffer = amv.create_buffer("test")
+        assert isinstance(buffer, Markdown)
+        message = amv.get_message()
+
+        assert message == {
+            "role": "assistant",
+            "content": "test",
+        }
+    except Exception:
+        pytest.fail("Method raised an exception unexpectedly!")
+
+
+def test_markdown_methods():
+    md = Markdown()
+    try:
+        md.append("test")
+        assert md.content == "test"
+        repr_md = repr(md)
+        assert repr_md == "test"
+    except Exception:
+        pytest.fail("Method raised an exception unexpectedly!")
